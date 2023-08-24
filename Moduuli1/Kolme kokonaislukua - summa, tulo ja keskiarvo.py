6e7fe80462f965e9532774bb7235b92d94ca0bd5
MarkusHieno <jmarkus.heino@gmail.com>
2023-08-24 17:14:23 +0300

    Kolme lukua

--- Moduuli1/Kolme kokonaislukua - summa, tulo ja keskiarvo.py
+++ Moduuli1/Kolme kokonaislukua - summa, tulo ja keskiarvo.py
@@ -6,5 +6,6 @@ sum = a+b+c
 product = a*b*c
 average= (a+b+c)/3
 
+
 print ("Näiden kolmen kokonaisuluvun summa on: ",sum)
-print ("Niiden tulo on:", product, " ja niiden keskiarvo on:", average), " ."
+print ("Niiden tulo on:", product, "ja niiden keskiarvo on:", average)
