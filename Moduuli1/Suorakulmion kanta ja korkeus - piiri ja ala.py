8f6db142f9ed770a0dd483c54eea99a82f4b0894
MarkusHieno <jmarkus.heino@gmail.com>
2023-08-24 15:43:02 +0300

    Testi Commit

---
+++ Moduuli1/Suorakulmion kanta ja korkeus - piiri ja ala.py
@@ -0,0 +1,6 @@
+b = int(input("Anna kolmion kannan pituus senttimetreinä : "))
+h = int(input("Anna kolmion korkeus senttimetreinä : "))
+
+area = b*h/2
+
+print("Kolmion pinta-ala on = ", area, "cm2")
