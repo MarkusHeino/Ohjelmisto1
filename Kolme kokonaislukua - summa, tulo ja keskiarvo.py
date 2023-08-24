78858a580676a707bab56ffeacb3baa71f35ce26
MarkusHieno <jmarkus.heino@gmail.com>
2023-08-24 17:04:06 +0300

    Kolme lukua

---
+++ Kolme kokonaislukua - summa, tulo ja keskiarvo.py
@@ -0,0 +1,10 @@
+a = int(input("Anna ensimmäinen kokonaisluku:"))
+b = int(input("Toinen: "))
+c = int(input("Ja kolmas: "))
+
+sum = a+b+c
+product = a*b*c
+average= (a+b+c)/3
+
+print ("Näiden kolmen kokonaisuluvun summa on: ",sum)
+print ("Niiden tulo on:", product, " ja niiden keskiarvo on:", average), " ."
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-install units
+
