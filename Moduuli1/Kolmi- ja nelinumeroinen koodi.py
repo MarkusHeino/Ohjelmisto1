cb0240a0199d66e5b42bc50a239a34911940d4c4
MarkusHieno <jmarkus.heino@gmail.com>
2023-08-24 23:23:50 +0300

    Keskiaikaiset painoyksiköt

---
+++ Moduuli1/Kolmi- ja nelinumeroinen koodi.py
@@ -0,0 +1,13 @@
+import random
+
+unit = input("Saisiko olla kolmen (3) vai neljän (4) numeron koodi?")
+
+if unit == "3":
+    print(random.randint(0,9), (random.randint(0,9), (random.randint(0,9))
+
+
+
+
+else:
+    print(f"{unit} Pahoittelut, anna komennoksi joko 3 tai 4!")
+
