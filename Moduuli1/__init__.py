e6fd293e406bd8770fc2cea33b4f2ce145bba0df
MarkusHieno <jmarkus.heino@gmail.com>
2023-08-24 14:54:28 +0300

    Testi Commit

---
+++ Moduuli1/__init__.py
@@ -0,0 +1,11 @@
+
+def print_hi(name):
+
+    print(f'Tervehdys, {name}')
+
+if __name__ == '__main__':
+    print_hi('käyttäjä!')
+
+name = input("Minä olen Python. Mikä sinun nimesi on?")
+
+print("Hei," + name+ "! Hauska tavata"+ "!")
