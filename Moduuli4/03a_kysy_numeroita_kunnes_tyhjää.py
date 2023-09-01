0716427b91905978502a3e67a5b18adcd198b86f
MarkusHeino <jmarkus.heino@gmail.com>
2023-09-01 14:58:41 +0300

    pelkkää vihreetä

---
+++ Moduuli4/03a_kysy_numeroita_kunnes_tyhjää.py
@@ -0,0 +1,23 @@
+isoin = None
+pienin = None
+
+while True:
+    num = input("Anna luku. Tyhjä lopettaa: ")
+    if num == "":
+        break
+    try:
+        num = int(num)
+    except:
+        print("Virheellinen arvo!")
+        continue
+    if isoin is None:
+        isoin = num
+    elif isoin < num:
+        isoin = num
+    if pienin is None:
+        pienin = num
+    elif num < pienin:
+        pienin = num
+
+print("Isoin numero oli ", isoin)
+print("Pienin numero oli", pienin)
