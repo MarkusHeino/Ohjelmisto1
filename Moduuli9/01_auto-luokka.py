3ecc8cd923a7e59fc0130766c12611f000203497
MarkusHeino <jmarkus.heino@gmail.com>
2023-10-23 12:03:58 +0300

    pelkkää vihreetä

---
+++ Moduuli9/01_auto-luokka.py
@@ -0,0 +1,12 @@
+class Auto:
+
+    tallissa = 0
+
+    def __init__(self, rekisteritunnus, huippunopeus, matka):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.matka = matka
+        Auto.tallissa = Auto.tallissa + 1
+
+auto1 = Auto("ABC-123", 142, 120000)
+print (f"Rekisterinumero on {auto1.rekisteritunnus} ja huippunopeus {auto1.huippunopeus} km/h.")
