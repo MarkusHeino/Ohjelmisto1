87390664a6bcf4592442d395bb520982f5e5a867
MarkusHieno <jmarkus.heino@gmail.com>
2023-11-22 08:39:52 +0200

    pyyttonia pyyttonia

--- Ohjelmisto2/Moduuli0_kokeeseen_treenit/3.koetehtävä_esimerkki.py
+++ Ohjelmisto2/Moduuli0_kokeeseen_treenit/3.koetehtävä_esimerkki.py
@@ -21,4 +21,40 @@ Flask on Pythonin web-kehys, joka helpottaa web-sovellusten kehittämistä.
 Se on kevyt, joustava ja helppokäyttöinen, ja se perustuu Werkzeugin WSGI-työkalupakettiin ja Jinja2-templaatointimoottoriin.
 Flask tarjoaa rakenteen web-sovellusten luomiseen ja mahdollistaa reitityksen, näkymien hallinnan, ja tiedonkäsittelyn.
 Flask on suosittu erityisesti pienten ja keskisuurten projektien kehityksessä.
+
+
+1. Luokka: Suunnitelma olioiden luomiseen. Se määrittelee joukon attribuutteja (muuttujia) ja metodeja (funktioita), joita oliot käyttävät.
+
+2. Olio: Luokan instanssi. Oliot sisältävät attribuutteja (dataa) ja käyttäytymistä (metodeja).
+
+3. Perintö: Luokan kyky periytyä attribuutteja ja metodeja toiselta luokalta.
+Se edistää koodin uudelleenkäyttöä ja mahdollistaa uuden luokan luomisen olemassa olevan perusteella.
+
+4. Kapselointi: Tietojen ja niihin liittyvien metodien niputtaminen yhteen yksikköön (luokka).
+Se auttaa rajoittamaan pääsyä oliokomponentteihin.
+
+5. Polymorfismi: Olioiden kyky ottaa useita muotoja.
+Pythonissa polymorfia saavutetaan usein metodin ylikuormituksen tai ylikirjoituksen avulla.
+
+6. Metodin Ylikuormitus: Useiden metodien määrittely samalla nimellä samassa luokassa, mutta eri parametreilla.
+Python ei tue perinteistä metodin ylikuormitusta, mutta voit saavuttaa samanlaisia tuloksia käyttämällä oletusarvoja tai muuttuvan pituuden argumenttilistoja.
+
+7. Metodin Ylikirjoitus: Erityisen toteutuksen tarjoaminen metodille alaluokassa, joka on jo määritelty sen yliluokassa.
+Tämä mahdollistaa aliluokan tarjoavan erityisen toteutuksen jo olemassa olevalle metodille.
+
+8. Abstraktio: Monimutkaisen todellisuuden piilottaminen ja paljastaminen vain välttämättömillä osilla.
+OOP:ssa abstraktio saavutetaan usein abstraktien luokkien ja rajapintojen avulla.
+
+9. Abstrakti Luokka: Luokka, jota ei voi instanssioida ja joka on tarkoitettu aliluokkien tekemiseen.
+Se voi sisältää abstrakteja metodeja, jotka on toteutettava sen aliluokissa.
+
+10. Rajapinta: Pythonissa rajapintoja ei ole määritelty nimenomaisesti, mutta käsite viittaa joukkoon metodeita, jotka luokan on toteutettava.
+Se on tapa määritellä sopimus luokille.
+
+11. Kompositio: Monimutkaisten olioiden rakentaminen yhdistämällä yksinkertaisempia oliota.
+Se edistää "omistaa" -suhdetta sen sijaan, että olisi "on" -suhde (kuten perinnössä).
+
+12. Getter- ja Setter-metodit: Metodeita, joita käytetään yksityisten attribuuttien arvojen hakemiseen ja asettamiseen,
+tarjoten valvottua pääsyä luokan attribuutteihin.
+
 '''
--- Ohjelmisto2/Moduuli0_kokeeseen_treenit/4.koetehtävä_esimerkki.py
+++ Ohjelmisto2/Moduuli0_kokeeseen_treenit/4.koetehtävä_esimerkki.py
@@ -1,26 +1,46 @@
+'''
+Kuva API-kutsusta, joka tuottaa seuraavan tulosteen selaimeen. Ilman prooffausta:
+
+from flask import Flask, Response
+app = Flask(__name__) @app.route(' ') def summa( ):
+
+# varsinainen koodi
+
+if __name__ == '__main__': app.run(use_reloader=True, host=' ', port= )
+
+Tuloste:
+{
+    "luku1: "17",
+    "luku2: "41",
+    "summa": 58
+}
+'''
+
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/summa')
 def summa():
-    # Simulate API parameters
+    # Simuloi API-parametrit
     luku1 = 17
     luku2 = 41
 
-    # Perform the calculation
+    # Tee laskenta
     summa = luku1 + luku2
 
-    # Prepare the response in JSON format
+    # Valmistele vastaus JSON-muodossa
     response_data = {
         "luku1": str(luku1),
         "luku2": str(luku2),
         "summa": summa
     }
 
-    # Return the response as JSON
+    # Palauta vastaus JSON-muodossa
     return jsonify(response_data)
 
 if __name__ == '__main__':
-    # Run the Flask application with specified parameters
+    # Käynnistä Flask-sovellus määritetyillä parametreilla
     app.run(use_reloader=True, host='0.0.0.0', port=5000)
+
